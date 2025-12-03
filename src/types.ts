0676ebb46b494d0ebbe3b385ce7548c015b6979c
abratko <abratko.github@gmail.com>
2025-12-03 17:37:15 +0300

    refactor(action): update ActionInner and ProtoModel to use generic types
    Refactored ActionInner and ProtoModel to utilize generic types for improved type safety and flexibility. Updated method signatures and type definitions to enhance compatibility with ProtoModel methods. Adjusted related tests to ensure proper functionality and validation of action states.

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ import { ActionInner } from './action'
 import { ProtoModel } from './proto-model'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type Action<Model extends ProtoModel, Args extends any[] = unknown[]> = Omit<ActionInner<Model, Args>, 'call'>
+export type Action<T extends ProtoModel, Args extends any[] = unknown[]> = Omit<ActionInner<T, Args>, 'call'>
 export type OriginalMethod = (...args: any[]) => Promise<void>
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -14,10 +14,10 @@ export interface OriginalMethodWrapper<Args extends any[] = unknown[]> {
 export type ActionStateName = keyof typeof ActionInner.possibleState
 
 export type ProtectedMethodInModel = 'action' | 'setActionState'
-export type Model<T> = {
+export type Model<T extends ProtoModel> = {
   [K in keyof T]:
     T[K] extends ((...args: infer Args) => Promise<void>)
-      ? Action<T extends ProtoModel ? T : never, Args>
+      ? Action<T, Args>
       : K extends ProtectedMethodInModel
         ? never
         : T[K]
