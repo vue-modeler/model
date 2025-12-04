908c7cfc81d2cbdccc28117698e61dfd63f3dd95
abratko <abratko.github@gmail.com>
2025-12-04 04:57:38 +0300

    refactor(action): update ActionInner tests to utilize model getter for improved functionality
    Refactored tests for the ActionInner class to leverage the new model getter functionality, enhancing the accuracy of action execution and error handling. Adjusted test cases to ensure proper instantiation and state management of actions within the context of a model, improving overall test coverage and reliability.

--- tests/action-constructor.spec.ts
+++ tests/action-constructor.spec.ts
@@ -1,29 +1,40 @@
 import { beforeEach, describe, expect, test } from 'vitest'
 import { ActionInner } from '../src/action'
 import { ActionInternalError } from '../src/error/action-internal-error'
-import { OriginalMethodWrapper } from '../src/types'
+import { Model, OriginalMethodWrapper } from '../src/types'
 import { TestProtoModel } from './test-model/test-proto-model'
 import { createApiMock } from './test-model/create-api-mock'
 
 describe('Action constructor', () => {
 
   let protoModel: TestProtoModel
+  let model: Model<TestProtoModel>
 
   beforeEach(() => {
     const api = createApiMock()
     protoModel = new TestProtoModel(api)
+    model = TestProtoModel.model(api, 'readonly')
   })
 
   test('successfully creates action outside a model, but the action will throw error in run time', () => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    const action = new ActionInner(protoModel, protoModel.successActionWithoutArgs as OriginalMethodWrapper<[]>) 
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    expect(() => { action.exec() }).toThrow(new Error('Action not found'))
+    const action = ActionInner.create(
+      protoModel,
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      protoModel.successActionWithoutArgs as OriginalMethodWrapper<[]>,
+      () => model
+    )
+    expect(() => {
+      void action.exec()
+    }).toThrow(new Error('Action not found'))
   })
 
   test('creates new instance for decorated action method with default state', () => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    const action = new ActionInner(protoModel, protoModel.successActionWithoutArgs as OriginalMethodWrapper<[]>)
+    const action = ActionInner.create(
+      protoModel,
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      protoModel.successActionWithoutArgs as OriginalMethodWrapper<[]>,
+      () => model
+    )
 
     expect(action.state).toBe(ActionInner.possibleState.ready)
     expect(action.toString()).toBe('successActionWithoutArgs')
@@ -38,13 +49,18 @@ describe('Action constructor', () => {
     expect(action.promise).toBe(null)
     expect(action.abortController).toBe(null)
     expect(action.args).toEqual([])
+    expect(action.owner).toBe(model)
   })
 
   test('throws error when method is not decorated as action', () => {
     expect.assertions(2)
     try {
-      // eslint-disable-next-line @typescript-eslint/unbound-method
-      new ActionInner(protoModel, protoModel.normalSyncMethodWithData as never as OriginalMethodWrapper<[]>)
+      new ActionInner(
+        protoModel,
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        protoModel.normalSyncMethodWithData as never as OriginalMethodWrapper<[]>,
+        () => model
+      )
     } catch (error) {
       expect(error).toBeInstanceOf(ActionInternalError)
       expect((error as ActionInternalError).message).toBe('Method normalSyncMethodWithData is not action')
@@ -58,8 +74,11 @@ describe('Action constructor', () => {
 
     expect.assertions(2)
     try {
-       
-      new ActionInner(protoModel, someFunction as never as OriginalMethodWrapper<[]>)
+      new ActionInner(
+        protoModel,
+        someFunction as never as OriginalMethodWrapper<[]>,
+        () => model
+      )
     } catch (error) {
       expect(error).toBeInstanceOf(ActionInternalError)
       expect((error as ActionInternalError).message).toBe('Model does not contain method someFunction')
